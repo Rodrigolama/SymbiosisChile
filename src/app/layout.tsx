08a9b102cb50ac7817f71fb9dcdad6be9f4149d5
Rodrigo Lama <rodrigolama@Rodrigos-MacBook-Air.local>
2026-03-18 14:09:36 -0300

    Add Google Tag Manager with GTM-K235XMRN

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,7 +26,32 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="es">
-      <body className={inter.className}>{children}</body>
+      <head>
+        {/* Google Tag Manager - Head */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-K235XMRN');
+            `,
+          }}
+        />
+      </head>
+      <body className={inter.className}>
+        {/* Google Tag Manager - Body (noscript) */}
+        <noscript
+          dangerouslySetInnerHTML={{
+            __html: `
+              <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-K235XMRN"
+              height="0" width="0" style="display:none;visibility:hidden"></iframe>
+            `,
+          }}
+        />
+        {children}
+      </body>
     </html>
   );
 }
